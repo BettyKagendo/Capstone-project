06a47d0e84969125c82a82f0ed1d21b4dc33a3f6
BettyKagendo <gitongabetty.k@gmail.com>
2024-05-23 14:23:20 +0300

    Establish a connecttion to database

--- main.py
+++ main.py
@@ -1,2 +1,14 @@
 import psycopg2
 from decouple import config
+
+#establish a connection
+try:
+    conn = psycopg2.connect(
+       dbname=config('database'),
+       user='postgres',
+       password=config('password'),
+       host='localhost'
+    )
+    print ('Connected to the  database')
+except psycopg2.Error as err:
+    print ("Unable to connect to the database: ", err)                                                         
