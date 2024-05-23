7640b93137a53b687a74b0f1cb4418adc27b4685
BettyKagendo <gitongabetty.k@gmail.com>
2024-05-23 14:34:24 +0300

    Define the product class

--- main.py
+++ main.py
@@ -11,4 +11,18 @@ try:
     )
     print ('Connected to the  database')
 except psycopg2.Error as err:
-    print ("Unable to connect to the database: ", err)                                                         
+    print ("Unable to connect to the database: ", err)  
+
+
+
+
+    #define the product class
+class Product:
+    def __init__(self, product_id, product_name, price, quantity):
+        self.product_id = product_id
+        self.product_name = product_name
+        self.price = price
+        self.quantity = quantity
+
+    def __str__(self):
+        return f"Product: {self.name}, Price: {self.price}, Quantity:{self.quantity}"                                                       
